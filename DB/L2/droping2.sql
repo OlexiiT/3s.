39e772e32a24e1d2418e7d0379640fcb65c25b33
Olexii <75754524+OlexiiT@users.noreply.github.com>
2021-12-27 15:09:42 +0200

    Create droping2.sql

---
+++ DB/L2/droping2.sql
@@ -0,0 +1,7 @@
+DROP TABLE student_discipline;
+DROP TABLE student_credit_module;
+DROP TABLE student;
+DROP TABLE credit_module;
+DROP TABLE discipline;
+DROP TABLE department;
+DROP TABLE faculty;
