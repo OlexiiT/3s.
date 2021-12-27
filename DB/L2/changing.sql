38719c328639b44588639143658638da48382756
Olexii <75754524+OlexiiT@users.noreply.github.com>
2021-12-27 19:43:10 +0200

    Update changing.sql

--- DB/L2/changing.sql
+++ DB/L2/changing.sql
@@ -1,6 +1,8 @@
 --1.
 ALTER TABLE student
 ADD email VARCHAR(20);
+ALTER TABLE student
+ADD age INT;
 --2.
 ALTER TABLE student
 ALTER COLUMN email TYPE VARCHAR(100);
@@ -12,34 +14,28 @@ ALTER TABLE student
 ALTER COLUMN email DROP NOT NULL;
 --5.
 ALTER TABLE student
-DROP COLUMN email;
-
-
-ALTER TABLE student
-ADD age INT;
-
-
+ADD CHECK (age > 0);
 --6.
 ALTER TABLE student
-ADD CHECK (age > 0);
+ADD UNIQUE (email);
 --7.
 ALTER TABLE student
-ADD UNIQUE (email);
+RENAME COLUMN phone_number TO phone;
 --8.
 ALTER TABLE student
-RENAME COLUMN phone_number TO phone;
---9.
-ALTER TABLE student
 RENAME TO person;
---10.
+--9.
 ALTER TABLE faculty
 ADD university VARCHAR(100);
---11.
+--10.
 ALTER TABLE department
 ADD age INT;
---12.
+--11.
 ALTER TABLE discipline
 ADD teacher VARCHAR(100);
---13.
+--12.
 ALTER TABLE credit_module
 ADD teacher VARCHAR(100);
+--13.
+ALTER TABLE person
+DROP COLUMN email;
