93609fc881a4a3db25080a6cbabdf89072823006
Olexii <75754524+OlexiiT@users.noreply.github.com>
2021-12-28 10:13:37 +0200

    Update L3.sql

--- DB/L3/L3.sql
+++ DB/L3/L3.sql
@@ -1,15 +1,14 @@
-
-
-/*1SELECT student_name, father_name, surname
+--1
+SELECT student_name, father_name, surname
 FROM student;
-2
+--2
 SELECT discipline_name, lection_hours, practic_hours, lab_hours, credits, attestation_type
 FROM discipline;
-3
+--3
 SELECT discipline_name, lection_hours, practic_hours, lab_hours, credits, attestation_type
 FROM discipline
 WHERE credits > 3;
-4
+--4
 SELECT discipline_name, lection_hours, practic_hours, lab_hours, credits, attestation_type
 FROM discipline
 WHERE credits > 3 AND department_id > 5;
@@ -36,9 +35,22 @@ WHERE student_name LIKE '%Оле%';
 --10
 SELECT student_name, father_name, surname
 FROM student
-WHERE student_name SIMILAR TO '%(О|Д)%';*/
+WHERE student_name SIMILAR TO '%(О|Д)%';
 --11
+SELECT student_name, father_name, surname
+FROM student
+WHERE adress IS NULL;
 --12
+SELECT faculty_name
+FROM faculty
+WHERE faculty_name LIKE '%Інститут%';
 --13
+SELECT discipline_name, credits
+FROM discipline
+WHERE discipline_name IN ('Іноземна мова', 'Основи програмування');
 --14
+SELECT * FROM department;
 --15
+SELECT student_name, father_name, surname
+FROM student
+WHERE student_name LIKE '%Микита%'
