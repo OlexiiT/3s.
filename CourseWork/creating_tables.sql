82459fa4042ca5c58979173347551bdadc7bb538
Olexii <75754524+OlexiiT@users.noreply.github.com>
2022-01-16 18:44:19 +0200

    Update creating_tables.sql

--- CourseWork/creating_tables.sql
+++ CourseWork/creating_tables.sql
@@ -5,10 +5,10 @@ CREATE TABLE city (
 
 CREATE TABLE office(
 	office_id SERIAL PRIMARY KEY,
-	city_id INTEGER FOREIGN KEY REFERENCES TO city (city_id),
+	city_id INTEGER REFERENCES city (city_id),
 	adress VARCHAR(100) NOT NULL UNIQUE,
 	phone VARCHAR(19) NOT NULL UNIQUE,
-	CHECK(phone LIKE "+38 (___) ___-__-__" OR phone LIKE "(___) ___-__-__")
+	CHECK(phone LIKE '+38 (___) ___-__-__' OR phone LIKE '(___) ___-__-__')
 );
 
 CREATE TABLE department(
@@ -19,33 +19,33 @@ CREATE TABLE department(
 
 CREATE TABLE employee(
 	employee_id SERIAL PRIMARY KEY,
-	office_id INTEGER NOT NULL FOREIGN KEY REFERENCES TO office(office_id),
+	office_id INTEGER NOT NULL REFERENCES office(office_id),
 	last_name VARCHAR(50) NOT NULL,
 	first_name VARCHAR(50) NOT NULL,
 	father_name VARCHAR(50) NOT NULL,
 	adress VARCHAR(100) NOT NULL,
-	phone VARCHAR(19) NOT NULL UNIQUE
+	phone VARCHAR(19) NOT NULL UNIQUE,
 	passport_number VARCHAR(30),
-	birthday DATE
-	email VARCHAR(50)
-	department_id INTEGER FOREIGN KEY REFERENCES TO department(department_id) NOT NULL
-	salary MONEY NOT NULL
-	CHECK(phone LIKE "+38 (___) ___-__-__" OR phone LIKE "(___) ___-__-__")
+	birthday DATE,
+	email VARCHAR(50),
+	department_id INTEGER REFERENCES department(department_id) NOT NULL,
+	salary MONEY NOT NULL,
+	CHECK(phone LIKE '+38 (___) ___-__-__' OR phone LIKE '(___) ___-__-__')
 );
 
 ALTER TABLE department 
-ADD FOREIGN KEY (manager_id) REFERENCES TO employee (emloyee_id);
+ADD FOREIGN KEY (manager_id) REFERENCES employee (employee_id);
 
 CREATE TABLE task(
 	task_id SERIAL PRIMARY KEY,
 	task_description VARCHAR(200),
 	task_cost MONEY,
-	department_id INTEGER NOT NULL FOREIGN KEY REFERENCES TO department(department_id)
+	department_id INTEGER NOT NULL REFERENCES department(department_id)
 );
 
 CREATE TABLE client(
 	client_id SERIAL PRIMARY KEY,
-	city_id INTEGER FOREIGN KEY REFERENCES TO city(city_id),
+	city_id INTEGER REFERENCES city(city_id),
 	last_name VARCHAR(50) NOT NULL,
 	first_name VARCHAR(50) NOT NULL,
 	father_name VARCHAR(50),
@@ -53,13 +53,13 @@ CREATE TABLE client(
 	phone VARCHAR(19) NOT NULL,
 	birthday DATE,
 	email VARCHAR(50) NOT NULL UNIQUE, 
-	CHECK(phone LIKE "+38 (___) ___-__-__" OR phone LIKE "(___) ___-__-__")
+	CHECK(phone LIKE '+38 (___) ___-__-__' OR phone LIKE '(___) ___-__-__')
 );
 
 CREATE TABLE orders(
 	order_id SERIAL PRIMARY KEY,
-	client_id INTEGER NOT NULL FOREIGN KEY REFERENCES TO client(client_id),
-	employee_id INTEGER NOT NULL FOREIGN KEY REFERENCES TO employee(employee_id),
+	client_id INTEGER NOT NULL REFERENCES client(client_id),
+	employee_id INTEGER NOT NULL REFERENCES employee(employee_id),
 	description VARCHAR(200),
 	date_start DATE,
 	date_end DATE
@@ -70,8 +70,8 @@ CREATE TABLE billboard(
 	adress VARCHAR(100),
 	date_end DATE,
 	phone VARCHAR(19) NOT NULL,
-	city_id INTEGER NOT NULL FOREIGN KEY REFERENCES TO city(city_id),
-	CHECK(phone LIKE "+38 (___) ___-__-__" OR phone LIKE "(___) ___-__-__")
+	city_id INTEGER NOT NULL REFERENCES city(city_id),
+	CHECK(phone LIKE '+38 (___) ___-__-__' OR phone LIKE '(___) ___-__-__')
 );
 
 CREATE TABLE tv_channel(
@@ -79,8 +79,8 @@ CREATE TABLE tv_channel(
 	channel_name VARCHAR(100),
 	adress  VARCHAR(100),
 	phone VARCHAR(19) NOT NULL,
-	city_id INTEGER NOT NULL FOREIGN KEY REFERENCES TO city(city_id),
-	CHECK(phone LIKE "+38 (___) ___-__-__" OR phone LIKE "(___) ___-__-__")
+	city_id INTEGER NOT NULL REFERENCES city(city_id),
+	CHECK(phone LIKE '+38 (___) ___-__-__' OR phone LIKE '(___) ___-__-__')
 );
 
 CREATE TABLE printery(
@@ -88,6 +88,6 @@ CREATE TABLE printery(
 	printery_name VARCHAR(50),
 	adress VARCHAR(100),
 	phone VARCHAR(19),
-	city_id INTEGER NOT NULL FOREIGN KEY REFERENCES TO city (city_id),
-	CHECK(phone LIKE "+38 (___) ___-__-__" OR phone LIKE "(___) ___-__-__")
+	city_id INTEGER NOT NULL REFERENCES city (city_id),
+	CHECK(phone LIKE '+38 (___) ___-__-__' OR phone LIKE '(___) ___-__-__')
 );
