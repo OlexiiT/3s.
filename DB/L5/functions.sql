7b54cee297b1dcc298c0cac2107e504bc033656c
Olexii <75754524+OlexiiT@users.noreply.github.com>
2022-01-17 22:42:21 +0200

    Update functions.sql

--- DB/L5/functions.sql
+++ DB/L5/functions.sql
@@ -7,3 +7,24 @@ END;
 $$ LANGUAGE plpgsql;
 
 SELECT getNumber();
+
+
+
+
+-- 2.c) Створити функцію, котра повертає таблицю заданої структури
+CREATE OR REPLACE FUNCTION get_3credit_disciplines() 
+RETURNS TABLE (
+	discipline_id INTEGER,
+	discipline_name VARCHAR(100),
+	credits INTEGER
+)
+LANGUAGE plpgsql
+AS
+$$ 
+BEGIN
+	RETURN QUERY
+		SELECT discipline.discipline_id, discipline.discipline_name, discipline.credits
+		FROM discipline AS discipline
+		WHERE discipline.credits = 3;
+END;
+$$;
