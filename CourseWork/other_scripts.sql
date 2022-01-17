8c029f2a863c8d216e8d5add4eeb8b07294584cb
Olexii <75754524+OlexiiT@users.noreply.github.com>
2022-01-17 17:18:18 +0200

    Update other_scripts.sql

--- CourseWork/other_scripts.sql
+++ CourseWork/other_scripts.sql
@@ -1,7 +1,21 @@
+CREATE PROCEDURE create_temp_table () AS
+$$
+BEGIN
+	CREATE TEMP TABLE IF NOT EXISTS employee_city AS
+	SELECT employee_id, last_name, first_name, father_name, employee.address, city_name
+	FROM employee INNER JOIN office ON employee.office_id = office.office_id
+	INNER JOIN city ON office.city_id = city.city_id;
+END;
+$$ LANGUAGE plpgsql;*/
+
+CALL create_temp_table();
+
+
 CREATE TABLE logs (
 	employee_id INTEGER NOT NULL,
 	added timestamp without time zone
 );
+
 
 CREATE OR REPLACE FUNCTION add_to_log() 
 RETURNS TRIGGER AS 
@@ -13,6 +27,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
 CREATE OR REPLACE TRIGGER add_trigger
 AFTER INSERT
 ON employee
@@ -22,4 +37,3 @@ EXECUTE PROCEDURE add_to_log();
 INSERT INTO employee (office_id, last_name, first_name, father_name, address, phone, passport_number, birthday, email, department_id, salary)
 VALUES (1, 'Тимошенко', 'Олексій', 'Олександрович', 'бул. Т. Шевченка 35', '(030) 234-23-44', '48126233', '2000-12-12', 'abc@gmail.com', 1, '19000');
 
---SELECT * FROM logs;
